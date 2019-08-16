bcaed832741904bdd962e764db1a372d21363b62
R2R1995 <mahdiabaszadeh@yahoo.com>
2019-08-16 15:12:07 +0430

    new commits

--- index.js
+++ index.js
@@ -1,5 +1,26 @@
-const t = test =>{
-    console.log(test)
-}
+const express  = require('express');
 
-t(123142)
+const app = express();
+
+app.set('view engine','ejs');
+app.set('views','views');
+
+app.listen(3000,(err)=>{
+    
+    if(err) console.log(err);
+    else console.log("APP STARTED ON PORT : 3000")
+})
+
+
+app.get('/api',(req,res)=>{
+
+    res.render('index.ejs',{
+        name: "Mohammad"
+    });
+})
+
+app.get('/test',(req,res)=>{
+    res.render('index.ejs',{
+        name:"Masoud"
+    });
+})
